90a881a8250d297debc7facce4e5f5cd7aac23d2
antoniosaints <costaantonio883@gmail.com>
2024-05-03 10:23:40 -0300

    ajustes essenciais

--- src/Views/template/main.php
+++ src/Views/template/main.php
@@ -1,11 +1,13 @@
 <?php
-include_once __DIR__ . '/../partials/contents_top.php'; 
-?>
+include_once __DIR__ . '/../partials/contents_top.php'; ?>
 <div class="w-auto">
     <?php include_once __DIR__ . '/../partials/header.php'; ?>
     <div class="mt-14 sm:ml-64">
-        <div class="content p-4 slide-it" id="content_main_page" hx-get="https://<?=$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]?>" hx-trigger="load delay:1s">
+        <div class="content p-4 slide-it" id="content_main_page" hx-trigger="load">
         </div>
     </div>
 </div>
+<script>
+        document.getElementById('content_main_page').setAttribute('hx-get', window.location.href);
+</script>
 <?php include_once __DIR__ . '/../partials/contents_end.php';
