25ba1efc4606fb3d5981905510c855c19341f36d
antoniosaints <costaantonio883@gmail.com>
2024-05-03 09:58:01 -0300

    ajustes essenciais

--- src/Views/caixas/lista_caixas.php
+++ src/Views/caixas/lista_caixas.php
@@ -19,8 +19,7 @@
         <nav aria-label="Page navigation example">
             <ul class="inline-flex -space-x-px text-sm">
                 <?php for ($i = 0; $i < $pages; $i++) {
-                    if ($i + 1 == $page) {
-                ?>
+                    if ($i + 1 == $page) { ?>
                         <li>
                             <a href="<?=APP_URL?>/caixas?page=<?= $i + 1 ?>" aria-current="page" class="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"><?= $i + 1 ?></a>
                         </li>
@@ -28,8 +27,7 @@
                         <li>
                             <a href="<?=APP_URL?>/caixas?page=<?= $i + 1 ?>" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"><?= $i + 1 ?></a>
                         </li>
-                <?php }
-                } ?>
+                <?php } } ?>
             </ul>
         </nav>
     </div>
