4adb9d0007d24b59f61d4c8146b616ff86ae9644
antoniosaints <costaantonio883@gmail.com>
2024-05-03 10:03:14 -0300

    ajustes essenciais

--- src/Views/template/main.php
+++ src/Views/template/main.php
@@ -3,7 +3,7 @@ include_once __DIR__ . '/../partials/contents_top.php'; ?>
 <div class="w-auto">
     <?php include_once __DIR__ . '/../partials/header.php'; ?>
     <div class="mt-14 sm:ml-64">
-        <div class="content p-4 slide-it" id="content_main_page" hx-get="<?=$_SERVER["REQUEST_URI"]?>" hx-trigger="load once">
+        <div class="content p-4 slide-it" id="content_main_page" >
         </div>
     </div>
 </div>
